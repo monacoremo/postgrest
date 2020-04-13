a662a0e628b54c263b5d3afdde21bd3038620c5d
Remo <remo>
2020-04-13 21:54:48 +0200

    Remove unneeded parts and reorder allColumns.

--- src/PostgREST/DbStructure.hs
+++ src/PostgREST/DbStructure.hs
@@ -393,130 +393,84 @@ allColumns tabs =
  H.Statement sql (arrayParam HE.text) (decodeColumns tabs) True
  where
   sql = [q|
-    SELECT DISTINCT
-        info.table_schema AS schema,
-        info.table_name AS table_name,
-        info.column_name AS name,
-        info.description AS description,
-        info.ordinal_position AS position,
-        info.is_nullable::boolean AS nullable,
-        info.data_type AS col_type,
-        info.is_updatable::boolean AS updatable,
-        info.character_maximum_length AS max_len,
-        info.numeric_precision AS precision,
-        info.column_default AS default_value,
-        array_to_string(enum_info.vals, ',') AS enum
-    FROM (
-        /*
-        -- CTE based on pg_catalog to get PRIMARY/FOREIGN key and UNIQUE columns outside api schema
-        */
-        WITH key_columns AS (
-             SELECT
-               r.oid AS r_oid,
-               c.oid AS c_oid,
-               n.nspname,
-               c.relname,
-               r.conname,
-               r.contype,
-               unnest(r.conkey) AS conkey
-             FROM
-               pg_catalog.pg_constraint r,
-               pg_catalog.pg_class c,
-               pg_catalog.pg_namespace n
-             WHERE
-               r.contype IN ('f', 'p', 'u')
-               AND c.relkind IN ('r', 'v', 'f', 'm')
-               AND r.conrelid = c.oid
-               AND c.relnamespace = n.oid
-               AND n.nspname <> ANY (ARRAY['pg_catalog', 'information_schema'] || $1)
-        ),
-        /*
-        -- CTE based on information_schema.columns
-        -- changed:
-        -- remove the owner filter
-        -- limit columns to the ones in the api schema or PK/FK columns
-        */
-        columns AS (
-            SELECT current_database()::information_schema.sql_identifier AS table_catalog,
-                nc.nspname::information_schema.sql_identifier AS table_schema,
-                c.relname::information_schema.sql_identifier AS table_name,
-                a.attname::information_schema.sql_identifier AS column_name,
-                d.description AS description,
-                a.attnum::information_schema.cardinal_number AS ordinal_position,
-                pg_get_expr(ad.adbin, ad.adrelid)::information_schema.character_data AS column_default,
-                    CASE
-                        WHEN a.attnotnull OR t.typtype = 'd'::"char" AND t.typnotnull THEN 'NO'::text
-                        ELSE 'YES'::text
-                    END::information_schema.yes_or_no AS is_nullable,
-                    CASE
-                        WHEN t.typtype = 'd'::"char" THEN
-                        CASE
-                            WHEN bt.typelem <> 0::oid AND bt.typlen = (-1) THEN 'ARRAY'::text
-                            WHEN nbt.nspname = 'pg_catalog'::name THEN format_type(t.typbasetype, NULL::integer)
-                            ELSE format_type(a.atttypid, a.atttypmod)
-                        END
-                        ELSE
-                        CASE
-                            WHEN t.typelem <> 0::oid AND t.typlen = (-1) THEN 'ARRAY'::text
-                            WHEN nt.nspname = 'pg_catalog'::name THEN format_type(a.atttypid, NULL::integer)
-                            ELSE format_type(a.atttypid, a.atttypmod)
-                        END
-                    END::information_schema.character_data AS data_type,
-                information_schema._pg_char_max_length(information_schema._pg_truetypid(a.*, t.*), information_schema._pg_truetypmod(a.*, t.*))::information_schema.cardinal_number AS character_maximum_length,
-                information_schema._pg_numeric_precision(information_schema._pg_truetypid(a.*, t.*), information_schema._pg_truetypmod(a.*, t.*))::information_schema.cardinal_number AS numeric_precision,
-                COALESCE(bt.typname, t.typname)::information_schema.sql_identifier AS udt_name,
-                CASE
-                    WHEN c.relkind = 'r'::"char" OR (c.relkind = ANY (ARRAY['v'::"char", 'f'::"char"])) AND pg_column_is_updatable(c.oid::regclass, a.attnum, false) THEN 'YES'::text
-                    ELSE 'NO'::text
-                END::information_schema.yes_or_no AS is_updatable
-            FROM pg_attribute a
-               LEFT JOIN key_columns kc ON kc.conkey = a.attnum AND kc.c_oid = a.attrelid
-               LEFT JOIN pg_catalog.pg_description AS d ON d.objoid = a.attrelid and d.objsubid = a.attnum
-               LEFT JOIN pg_attrdef ad ON a.attrelid = ad.adrelid AND a.attnum = ad.adnum
-               JOIN (pg_class c
-               JOIN pg_namespace nc ON c.relnamespace = nc.oid) ON a.attrelid = c.oid
-               JOIN (pg_type t
-               JOIN pg_namespace nt ON t.typnamespace = nt.oid) ON a.atttypid = t.oid
-               LEFT JOIN (pg_type bt
-               JOIN pg_namespace nbt ON bt.typnamespace = nbt.oid) ON t.typtype = 'd'::"char" AND t.typbasetype = bt.oid
-               LEFT JOIN (pg_collation co
-               JOIN pg_namespace nco ON co.collnamespace = nco.oid) ON a.attcollation = co.oid AND (nco.nspname <> 'pg_catalog'::name OR co.collname <> 'default'::name)
-            WHERE
-                NOT pg_is_other_temp_schema(nc.oid)
-                AND a.attnum > 0
-                AND NOT a.attisdropped
-                AND (c.relkind = ANY (ARRAY['r'::"char", 'v'::"char", 'f'::"char", 'm'::"char"]))
-                AND (nc.nspname = ANY ($1) OR kc.r_oid IS NOT NULL) /*--filter only columns that are FK/PK or in the api schema */
-              /*--AND (pg_has_role(c.relowner, 'USAGE'::text) OR has_column_privilege(c.oid, a.attnum, 'SELECT, INSERT, UPDATE, REFERENCES'::text))*/
-        )
-        SELECT
-            table_schema,
-            table_name,
-            column_name,
-            description,
-            ordinal_position,
-            is_nullable,
-            data_type,
-            is_updatable,
-            character_maximum_length,
-            numeric_precision,
-            column_default,
-            udt_name
-        /*-- FROM information_schema.columns*/
-        FROM columns
-        WHERE table_schema NOT IN ('pg_catalog', 'information_schema')
-    ) AS info
-    LEFT OUTER JOIN (
-        SELECT
-            n.nspname AS s,
-            t.typname AS n,
-            array_agg(e.enumlabel ORDER BY e.enumsortorder) AS vals
-        FROM pg_type t
-        JOIN pg_enum e ON t.oid = e.enumtypid
-        JOIN pg_catalog.pg_namespace n ON n.oid = t.typnamespace
-        GROUP BY s,n
-    ) AS enum_info ON (info.udt_name = enum_info.n)
-    ORDER BY schema, position |]
+    select distinct
+        relnamespace.nspname::text as schema,
+        pg_class.relname::text as table_name,
+        pg_attribute.attname::text as name,
+        pg_description.description::text as description,
+        pg_attribute.attnum::integer as position,
+        
+        (not (
+            pg_attribute.attnotnull
+            or pg_type.typtype = 'd' -- domain
+            and pg_type.typnotnull
+        ))::bool as nullable,
+
+        case
+            when pg_type.typtype = 'd' then    
+                case
+                    when basetype.typelem <> 0::oid and basetype.typlen = (-1) then
+                        'ARRAY'
+                    when basetypenamespace.nspname = 'pg_catalog'::name then
+                        format_type(pg_type.typbasetype, null)
+                    else
+                        format_type(pg_attribute.atttypid, pg_attribute.atttypmod)
+                end
+            else
+                case
+                    when pg_type.typelem <> 0::oid and pg_type.typlen = (-1) then
+                        'ARRAY'
+                    when typnamespace.nspname = 'pg_catalog'::name then
+                        format_type(pg_attribute.atttypid, null)
+                    else
+                        format_type(pg_attribute.atttypid, pg_attribute.atttypmod)
+                end
+        end::text as col_type,
+
+        (
+            pg_class.relkind = any (array['r', 'v', 'f']) 
+            and pg_column_is_updatable(pg_class.oid::regclass, pg_attribute.attnum, false)
+        )::bool as updateable,
+        
+        information_schema._pg_char_max_length(typid, typmod)::integer as max_len,
+        information_schema._pg_numeric_precision(typid, typmod)::integer as precision,
+        pg_get_expr(pg_attrdef.adbin, pg_attrdef.adrelid)::text as default_value,
+        array_to_string(enum.vals, ',')::text as enum
+    from
+        pg_attribute
+        join pg_class 
+            on pg_class.oid = pg_attribute.attrelid
+        join pg_namespace relnamespace 
+            on relnamespace.oid = pg_class.relnamespace
+        join pg_type 
+            on pg_type.oid = pg_attribute.atttypid
+        join pg_namespace typnamespace 
+            on typnamespace.oid = pg_type.typnamespace 
+        left join pg_description
+            on pg_description.objoid = pg_attribute.attrelid
+            and pg_description.objsubid = pg_attribute.attnum
+        left join pg_attrdef
+            on pg_attrdef.adrelid = pg_attribute.attrelid 
+            and pg_attrdef.adnum = pg_attribute.attnum
+        left join pg_type basetype 
+            on basetype.oid = pg_type.typbasetype
+        left join pg_namespace basetypenamespace 
+            on basetypenamespace.oid = basetype.typnamespace,
+        lateral (
+            select array_agg(pg_enum.enumlabel order by pg_enum.enumsortorder) as vals
+            from pg_enum
+            where pg_type.oid = pg_enum.enumtypid 
+        ) enum,
+        information_schema._pg_truetypid(pg_attribute.*, pg_type.*) as typid,
+        information_schema._pg_truetypmod(pg_attribute.*, pg_type.*) as typmod
+    where
+        not pg_is_other_temp_schema(relnamespace.oid)
+        and pg_attribute.attnum > 0
+        and not pg_attribute.attisdropped
+        and pg_class.relkind = any (array['r', 'v', 'f', 'm'])
+        and relnamespace.nspname not in ('pg_catalog', 'information_schema')
+    order by
+        schema, table_name, position |]
 
 columnFromRow :: [Table] ->
                  (Text,        Text,        Text,
