b2272208e67da98dabefc861c2e1c3d6f2aeec78
Remo <remo>
2020-04-27 20:21:08 +0200

    Progress, but still fails for the packages that required -fexternal-interpreter

--- nix/cross.nix
+++ nix/cross.nix
@@ -6,9 +6,33 @@ let
   overlay =
     self: super:
       let
+        lib = self.haskell.lib;
+
+        addExternalInterpreter =
+          drv:
+            lib.appendConfigureFlags
+              drv
+              [ "--ghc-options=-fexternal-interpreter" ];
+
         overrides =
           final: prev: {
             "${name}" = prev.callCabal2nix name src {};
+
+            # The following packages fail to build with Haddock, apparently
+            # due to it panicing when trying to run coverage.
+            binary-parser = lib.dontHaddock prev.binary-parser;
+            cmdargs = lib.dontHaddock prev.cmdargs;
+            vector-builder = lib.dontHaddock prev.vector-builder;
+            QuickCheck = lib.dontHaddock prev.QuickCheck;
+            bytestring-strict-builder = lib.dontHaddock prev.bytestring-strict-builder;
+            bytestring-tree-builder = lib.dontHaddock prev.bytestring-tree-builder;
+            loch-th = lib.dontHaddock prev.loch-th;
+
+            data-dword = addExternalInterpreter prev.data-dword;
+            generics-sop = addExternalInterpreter prev.generics-sop;
+            th-orphans = addExternalInterpreter prev.th-orphans;
+            math-functions = addExternalInterpreter prev.th-orphans;
+            jose = addExternalInterpreter prev.jose;
           };
       in
         {
