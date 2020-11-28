0d7f2ea4de79bdd2176ba614463de2c59b976374
Remo Rechkemmer <59358383+monacoremo@users.noreply.github.com>
2020-11-28 22:48:13 +0100

    Update devtools.nix

--- nix/devtools.nix
+++ nix/devtools.nix
@@ -3,45 +3,10 @@
 , checkedShellScript
 , entr
 , git
-, hlint
-, nixpkgs-fmt
-, silver-searcher
-, stylish-haskell
+, style
 , tests
 }:
 let
-  style =
-    checkedShellScript "postgrest-style"
-      ''
-        rootdir="$(${git}/bin/git rev-parse --show-toplevel)"
-
-        # Format Nix files
-        ${nixpkgs-fmt}/bin/nixpkgs-fmt "$rootdir" > /dev/null 2> /dev/null
-
-        # Format Haskell files
-        ${silver-searcher}/bin/ag -l -g '\.l?hs$' . "$rootdir" \
-          | xargs ${stylish-haskell}/bin/stylish-haskell -i
-      '';
-
-  # Script to check whether any uncommited changes result from postgrest-style
-  styleCheck =
-    checkedShellScript "postgrest-style-check"
-      ''
-        ${style}
-
-        ${git}/bin/git diff-index --exit-code HEAD -- '*.hs' '*.lhs' '*.nix'
-      '';
-
-  lint =
-    checkedShellScript "postgrest-lint"
-      ''
-        rootdir="$(${git}/bin/git rev-parse --show-toplevel)"
-
-        # Lint Haskell files
-        ${silver-searcher}/bin/ag -l -g '\.l?hs$' "$rootdir" \
-          | xargs ${hlint}/bin/hlint -X QuasiQuotes -X NoPatternSynonyms
-      '';
-
   watch =
     checkedShellScript "postgrest-watch"
       ''
@@ -76,16 +41,14 @@ let
   check =
     checkedShellScript "postgrest-check"
       ''
-        ${style}
         ${tests}/bin/postgrest-test-spec-all
+        ${style}/bin/postgrest-lint
+        ${style}/bin/postgrest-style-check
       '';
 in
 buildEnv {
   name = "postgrest-devtools";
   paths = [
-    style.bin
-    styleCheck.bin
-    lint.bin
     watch.bin
     pushCachix.bin
     build.bin
