195f8da8767d2ef6cf4d8b12054e3c1cbcbc60ad
monacoremo <59358383+monacoremo@users.noreply.github.com>
2021-04-08 19:36:55 +0200

    update nixpkgs

--- nix/nixpkgs-version.nix
+++ nix/nixpkgs-version.nix
@@ -1,6 +1,6 @@
-# Pinned version of Nixpkgs, generated with nixpkgs-upgrade.
+# Pinned version of Nixpkgs, generated with postgrest-nixpkgs-upgrade.
 {
-  date = "2020-12-22";
-  rev = "2a058487cb7a50e7650f1657ee0151a19c59ec3b";
-  tarballHash = "1h8c0mk6jlxdmjqch6ckj30pax3hqh6kwjlvp2021x3z4pdzrn9p";
+  date = "2021-04-08";
+  rev = "4cb48cc25622334f17ec6b9bf56e83de0d521fb7";
+  tarballHash = "0z005p4jwlnfh9gbgjc3anzrabzdys2d94l4chvdhzxr1pyj4imy";
 }
